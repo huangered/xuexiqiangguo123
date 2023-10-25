baeeaa01d1cdff618a52d0eabd8d9e84e1e4d32b
peter huang <peter.huang@arm.com>
2023-10-25 09:09:17 +0800

    Test

---
+++ go.py
@@ -0,0 +1,69 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+import time
+
+
+# read file
+#f = open("all.txt", "rw")
+
+option = webdriver.ChromeOptions()
+
+option.add_argument('--user-data-dir=C:\\Users\\pethua01\\AppData\\Local\\Google\\Chrome\\User Data')
+option.add_argument("--no-sandbox")
+option.add_argument("--disable-dev-shm-usage")
+option.add_argument('--disable-gpu')
+#option.add_argument("--headless")
+
+driver = webdriver.Chrome(options=option)
+
+driver.get("https://www.xuexi.cn")
+
+driver.maximize_window()
+
+print("max")
+
+time.sleep(5)
+
+print("读文章")
+
+# 每天中午操作。
+docs = driver.find_elements(By.CLASS_NAME, "text-link-item-title")
+
+print(docs)
+
+links = []
+
+for doc in docs:
+    doc1 = doc.find_element(By.CLASS_NAME, "text-wrap").find_element(By.CLASS_NAME, "text")
+    if len(doc1.text) > 6:
+        links.append(doc1);
+        print("add %s\r\n", doc1.text)
+
+for i in range (0, 10):
+    doc1 = links[i]
+    doc1.click()
+
+
+time.sleep(3)
+
+windows = driver.window_handles
+
+print(windows)
+for i in range (1, 11):
+    driver.switch_to.window(windows[i])
+    time.sleep(5)
+    voice = driver.find_element(By.CLASS_NAME, "voice-lang-switch")
+    if voice != None:
+        print("voice\r\n")
+        voice.click()
+    time.sleep(70)
+    
+
+    
+print("看视频")
+    
+    
+    
+    
+time.sleep(1000000)
